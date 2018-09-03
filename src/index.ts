875b8d8e7a93287ac90ec5d4d78048a6d976fff2
dudco <dudco1129@naver.com>
2018-09-03 21:03:20 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/PetFeed", {useNewUrlParser: true}).then(() => {}).catch(err => {
+    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+});
+
+app.set("port", process.env.PORT || 3000);
+app.set("jwt-secret", process.env.JWT_SECRET || "PETFEEDZZANG");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public/"));
+
+
+// routes
+
+app.listen(app.get("port"), () => {
+    console.log("server running at %d port", app.get("port"));
+});
